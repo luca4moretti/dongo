96ead9b703a9356aa3ab30bb7a3253494b52e889
luca4moretti <luca4moretti@proton.me>
2026-07-30 16:30:32 +0300

    Create logger.js

---
+++ middleware/logger.js
@@ -0,0 +1,27 @@
+module.exports = (
+
+    req,
+
+    res,
+
+    next
+
+) => {
+
+    const now =
+
+        new Date().toISOString();
+
+    console.log(
+
+        `[${now}]`,
+
+        req.method,
+
+        req.originalUrl
+
+    );
+
+    next();
+
+};
